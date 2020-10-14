fa37840536f12cad7593b7b02e790ca63e3f8792
aliaydins <ali.aydinn61@gmail.com>
2020-10-14 13:21:35 +0300

    basic permission based auth

--- src/main/java/com/example/security/demo/securitydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/demo/securitydemo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.security.demo.securitydemo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import static com.example.security.demo.securitydemo.security.ApplicationUserRole.*;
+import static com.example.security.demo.securitydemo.security.ApplicationUserPermission.*;
+
 
 @Configuration
 @EnableWebSecurity
@@ -27,9 +31,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http
+               .csrf().disable()
                .authorizeRequests()
                .antMatchers("/","index","/css/*","/js/*").permitAll()
-               .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
+               .antMatchers("/api/**").hasRole(STUDENT.name())
+               .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+               .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+               .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+               .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
                .anyRequest()
                .authenticated()
                .and()
@@ -42,16 +51,25 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(ApplicationUserRole.STUDENT.name()) //ROLE_STUDENT
+                //.roles(ApplicationUserRole.STUDENT.name()) //ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
         UserDetails lindaUser=User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ApplicationUserRole.ADMIN.name())
+                //.roles(ApplicationUserRole.ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+        UserDetails tomUser=User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+                //.roles(ApplicationUserRole.ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
         return new InMemoryUserDetailsManager(
                 annaSmithUser,
-                lindaUser
+                lindaUser,
+                tomUser
         );
     }
 }
